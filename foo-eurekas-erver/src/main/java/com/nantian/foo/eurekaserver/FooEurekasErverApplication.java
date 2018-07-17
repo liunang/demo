d4955fa9daf90b03a1960d242d054a727f5db557
柳楠楠 <liunang1989>
2018-07-17 22:20:04 +0800

    1、修复机构树无法正常加载得错误。

---
+++ foo-eurekas-erver/src/main/java/com/nantian/foo/eurekaserver/FooEurekasErverApplication.java
@@ -0,0 +1,14 @@
+package com.nantian.foo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FooEurekasErverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooEurekasErverApplication.class, args);
+    }
+}
