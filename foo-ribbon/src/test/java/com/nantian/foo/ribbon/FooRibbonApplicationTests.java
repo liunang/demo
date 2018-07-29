156427a6dbbbae7165ff512573076478e8f4889f
柳楠楠 <liunang1989>
2018-07-29 15:22:08 +0800

    1、新建ribbon

---
+++ foo-ribbon/src/test/java/com/nantian/foo/ribbon/FooRibbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nantian.foo.ribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FooRibbonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
